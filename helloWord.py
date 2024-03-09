e6f3a6e4f33ee7bb997b845ebc9268a17803baba
Geovanny Lara <jeffersonlara98@gmail.com>
2024-03-08 22:41:10 -0500

    Corregir errores de fusión

--- helloWord.py
+++ helloWord.py
@@ -1,7 +1,9 @@
 def welcome_message():
     name = input("Por favor ingrese su nombre: ")
     ciudad = input("¿De dónde eres? ")
+    atencion = input("¿Qué fue lo que más te llamó la atención del curso? ")
     print("Bienvenido,", name, "al curso de Django y React!")
+    print("Esperamos que disfrutes tu estadía en", ciudad, "y que aprendas mucho sobre", atencion)
 
 if __name__ == "__main__":
     welcome_message()
