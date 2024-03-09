392770c7235e86dc57332ee7a4392537efbf76e9
Geovanny Lara <jeffersonlara98@gmail.com>
2024-03-08 22:23:32 -0500

    Agregar funcionalidad de conocer la ciudad

--- helloWord.py
+++ helloWord.py
@@ -1,5 +1,6 @@
 def welcome_message():
     name = input("Por favor ingrese su nombre: ")
+    ciudad = input("¿De dónde eres? ")
     print("Bienvenido,", name, "al curso de Django y React!")
 
 if __name__ == "__main__":
